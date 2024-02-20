0327cbf8a725d16f0ab5258d7f04a3a25e72ec92
EderKeoma <107501828+EderKeoma@users.noreply.github.com>
2024-02-20 08:37:25 -0600

    Version 0.0
    Expo aplication created

---
+++ Triolingo-app/App.js
@@ -0,0 +1,12 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+import { NativeBaseProvider, Box } from "native-base";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Box></Box>
+    </NativeBaseProvider>
+  );
+}
---
+++ Triolingo-app/src/components/Login.js
@@ -0,0 +1,6 @@
+import {useForm, Coroller} from 'react-hook-form';
+
+export default function LoginForm(props){
+    const {control, handleSubmit, errors, setValue} = useForm();
+    const onSubmit = (data) =>[];
+};
