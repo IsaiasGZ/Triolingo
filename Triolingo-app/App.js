2db1e9c2ac1646af96e931ff06959f7441aed33c
LezoAngel <152739921+AngelLezozj@users.noreply.github.com>
2024-03-04 07:56:42 -0600

    API version 1
    Se realizó la API, además, se hizo la conexión a Mongodb y al proyecto principal

--- Triolingo-app/App.js
+++ Triolingo-app/App.js
@@ -1,5 +1,18 @@
+<<<<<<< Updated upstream
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
+=======
+import LoginScreen from './src/components/Login';
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import RegisterScreen from './src/components/Register';
+import NavigationBar from './src/components/Nav-bar';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import axios from 'axios';
+>>>>>>> Stashed changes
 
 import { NativeBaseProvider, Box } from "native-base";
 
--- Triolingo-app/src/components/Login.js
+++ Triolingo-app/src/components/Login.js
@@ -1,6 +1,66 @@
+<<<<<<< Updated upstream
 import {useForm, Coroller} from 'react-hook-form';
 
 export default function LoginForm(props){
     const {control, handleSubmit, errors, setValue} = useForm();
     const onSubmit = (data) =>[];
+=======
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, TextInput, Button, Alert, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+
+const LoginScreen = ({ navigation }) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Por ahora, solo mostraremos las datos ingresadas
+    Alert.alert('Credenciales', `Correo electrónico: ${email}, Contraseña: ${password}`);
+  };
+
+  const handleRegisterLink = () => {
+    navigation.navigate('Register');
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <Image
+        // source={require('./assets/muerdeaki.jpg')}
+        style={styles.image}
+        />
+      
+      <Text style={styles.title}>Inicio de sesión</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Correo electrónico"
+        onChangeText={text => setEmail(text)}
+        value={email}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        onChangeText={text => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <Button
+
+        title="Iniciar sesión"
+        onPress={handleLogin}
+        color={'#81779B'}
+      />
+
+<TouchableOpacity onPress={handleRegisterLink}>
+        <Text style={styles.registerLink}>¿No tienes una cuenta? ¡Crea una aquí!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+>>>>>>> Stashed changes
 };
