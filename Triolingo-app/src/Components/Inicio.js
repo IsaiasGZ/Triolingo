fd667e24959f532d90496e39519ce9ee12496cfa
EderKeoma <107501828+EderKeoma@users.noreply.github.com>
2024-03-31 13:46:58 -0600

    home v0.2.1
    Se agregaros mas screens

--- Triolingo-app/src/Components/Inicio.js
+++ Triolingo-app/src/Components/Inicio.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native'; 
+import HomeScreen from '../Screens/Home';
+import NivelesScreen from './NivelesScreen';
+import LeccionScreen from '../Screens/Lecciones';
+import PerfilScreen from './PerfilScreen';
 
 const Drawer = createDrawerNavigator(); 
 
@@ -10,11 +14,11 @@ const InicioDrawerScreen = () => {
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Niveles" component={NivelesScreen} /> 
-        <Drawer.Screen name="Lecciones" component={LeccionesScreen} /> 
+        <Drawer.Screen name="Lecciones" component={LeccionScreen} /> 
         <Drawer.Screen name="Perfil" component={PerfilScreen} /> 
       </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
-export default  InicioDrawerScreen; 
+export default InicioDrawerScreen;
